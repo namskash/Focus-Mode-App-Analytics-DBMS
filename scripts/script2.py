9f0b3125b5482c37b62140955cf902a05206a8ac
Naman Kashyap <107040799+namskash@users.noreply.github.com>
2022-10-20 23:49:24 +0530

    APPS table insertion

--- scripts/script2.py
+++ scripts/script2.py
@@ -1,4 +1,7 @@
 import mysql.connector
+from datetime import date, timedelta
+from random import choices
+from random import randint
 
 mydb = mysql.connector.connect(
 	host="localhost",
@@ -9,8 +12,38 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor(buffered=True)
 
-f = open('create_tables.sql')
+# SESSIONS + BREAKS
+date1, date2 = date(2021,11,4), date.today()
+dates = [date1]
+while(date1 <= date2):
+	date1 += timedelta(days = randint(0,4))
+	dates.append(date1)
+	#print(date1)
 
-sql_as_string = f.read()
+dates = list(choices(dates,k=100))	# k=number of dates needed
+#print(len(dates))
 
-mycursor.execute(sql_as_string)
+sessionID = 1
+sessionDurations = [30,45,60,75,90,105,120,135,150,165,180]	# all in minutes
+sessionStartTimes = 0
+
+# APPS
+sessionAppList = ['MS Word','MS PowerPoint','MS Excel','VS Code','Eclipse','Oracle VirtualBox','Ubuntu 22.04 VM']
+breakAppList = ['Google Chrome','YouTube','Prime Video','Netflix','Solitaire','Hangman']
+
+for i in range(len(sessionAppList)):
+	appID = "APP" + "0" * (3 - len(str(i+1))) + str(i+1)
+	mycursor.execute("insert into APPS values (%s,%s,%s,%s,%s)",(appID,sessionAppList[i],1,None,None))
+
+id = len(sessionAppList) + 1
+for i in range(len(breakAppList)):
+	appID = "APP" + "0" * (3 - len(str(i+id))) + str(i+id)
+	mycursor.execute("insert into APPS values (%s,%s,%s,%s,%s)",(appID,breakAppList[i],0,None,None))
+
+mydb.commit()
+
+mycursor.execute("select * from APPS")
+res = mycursor.fetchall()
+
+for i in res:
+	print(i)
