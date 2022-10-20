ce2247591fd4ec01eb6c557aa5b49239f4cf4534
Naman Kashyap <107040799+namskash@users.noreply.github.com>
2022-10-20 23:16:51 +0530

    tweaks
    Added appName and changed all IDs to varchar from int

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -3,7 +3,7 @@ create database focusModeDBMS;
 use focusModeDBMS;
 
 create table SESSIONS (
-	sessionID int not null,
+	sessionID varchar(10) not null,
 	sessionDate date not null,
 	startTime time not null,
 	endTime time not null,
@@ -12,18 +12,19 @@ create table SESSIONS (
 );
 
 create table BREAKS (
-	breakID int not null,
+	breakID varchar(10) not null,
 	breakDate date not null,
 	startTime time not null,
 	endTime time not null,
 	allowedDuration varchar(20) not null,
-	sessionID int not null,
+	sessionID varchar(10) not null,
 	primary key (breakID),
 	foreign key (sessionID) references SESSIONS(sessionID)
 );
 
 create table APPS (
-	appID int not null,
+	appID varchar(10) not null,
+	appName varchar(50) not null,
 	privileged bit not null,
 	timer varchar(20),
 	timerMAX varchar(20),
@@ -31,22 +32,22 @@ create table APPS (
 );
 
 create table WARNINGS (
-	warningID int not null,
+	warningID varchar(10) not null,
 	warningDate date not null,
 	snooze varchar(20),
-	appID int not null,
+	appID varchar(10) not null,
 	primary key (warningID),
 	foreign key (appID) references APPS(appID)
 );
 
 create table SESSION_APPS (
-	sessionID int not null,
-	appID int not null,
+	sessionID varchar(10) not null,
+	appID varchar(10) not null,
 	primary key (sessionID, appID)
 );
 
 create table BREAK_APPS (
-	breakID int not null,
-	appID int not null,
+	breakID varchar(10) not null,
+	appID varchar(10) not null,
 	primary key (breakID, appID)
 );
